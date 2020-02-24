7b6633277592a9df10b612977555feddf19002ba
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-02-24 11:03:46 -0500

    Fix issues with not placing markers at edit cursor when stopped.

--- VoiceOverMarking/MarkSectionBad.lua
+++ VoiceOverMarking/MarkSectionBad.lua
@@ -1,13 +1,7 @@
-pause = 2
-play = 1
-record = 4
-
-play_state = reaper.GetPlayState()
-if play_state == 0 then
+if reaper.GetPlayState() == 0 then
   cur_pos = reaper.GetCursorPosition()
 else
   cur_pos = reaper.GetPlayPosition()
 end
-reaper.ShowConsoleMsg(tostring(play_state) .. " " .. tostring(cur_pos) .. " " .. tostring(play_pos) .. "\n")
 reaper.AddProjectMarker2(0, false, cur_pos, -1, "B", -1, reaper.ColorToNative(255, 0, 0)|16777216)
 
