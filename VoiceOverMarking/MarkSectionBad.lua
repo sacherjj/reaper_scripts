8e720ce01af8b5541091380ab1c8277366952954
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-02-24 10:59:20 -0500

    Fix issues with not placing markers at edit cursor when stopped.

--- VoiceOverMarking/MarkSectionBad.lua
+++ VoiceOverMarking/MarkSectionBad.lua
@@ -1,3 +1,13 @@
-cur_time = reaper.GetPlayPosition()
-index = reaper.AddProjectMarker2(0, false, cur_time, -1, "B", -1, reaper.ColorToNative(255, 0, 0)|16777216)
+pause = 2
+play = 1
+record = 4
+
+play_state = reaper.GetPlayState()
+if play_state == 0 then
+  cur_pos = reaper.GetCursorPosition()
+else
+  cur_pos = reaper.GetPlayPosition()
+end
+reaper.ShowConsoleMsg(tostring(play_state) .. " " .. tostring(cur_pos) .. " " .. tostring(play_pos) .. "\n")
+reaper.AddProjectMarker2(0, false, cur_pos, -1, "B", -1, reaper.ColorToNative(255, 0, 0)|16777216)
 
--- VoiceOverMarking/MarkSectionStart.lua
+++ VoiceOverMarking/MarkSectionStart.lua
@@ -1,3 +1,6 @@
-cur_time = reaper.GetPlayPosition()
-index = reaper.AddProjectMarker2(0, false, cur_time, -1, "S", -1, reaper.ColorToNative(0, 0, 255)|16777216)
-
+if reaper.GetPlayState() == 0 then
+  cur_pos = reaper.GetCursorPosition()
+else
+  cur_pos = reaper.GetPlayPosition()
+end
+reaper.AddProjectMarker2(0, false, cur_pos, -1, "S", -1, reaper.ColorToNative(0, 0, 255)|16777216)
